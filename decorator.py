2e19bf59d1760ec4727a14d2ea1cedbbdb1b1b02
rnaster <blpomspo@gmail.com>
2019-11-19 18:27:15 +0900

    using functools
    - lru_cache
    - wraps

--- decorator.py
+++ decorator.py
@@ -3,6 +3,7 @@ decorator
 when compiling, f1 and f2 are decorated
 """
 import time
+import functools
 registry = []
 
 
@@ -19,6 +20,26 @@ def clock(func):
         elapsed = time.perf_counter() - t0
         name = func.__name__
         arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+
+    return clocked
+
+
+def clock2(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
         print('[%.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
         return result
 
--- ex_decorator.py
+++ ex_decorator.py
@@ -2,6 +2,7 @@
 decorator test
 """
 import time
+import functools
 import decorator as deco
 
 
@@ -37,6 +38,26 @@ def factorial(n):
     return 1 if n < 2 else factorial(n - 1) * n
 
 
+@deco.clock2
+def power(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    tmp = power(a, b // 2)
+    if b % 2:
+        return tmp * tmp * a
+    return tmp * tmp
+
+
+@functools.lru_cache()
+@deco.clock2
+def fibo(n):
+    if n < 2:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+
 if __name__ == '__main__':
     main()
     print('*' * 40, 'Calling snooze(.123)')
@@ -44,3 +65,9 @@ if __name__ == '__main__':
     print('*' * 40, 'Calling factorial(6)')
     print('6! =', factorial(6))
     print(factorial.__name__)
+    print('*' * 40, 'Calling power(2, 20)')
+    print('2 ** 20! =', power(2, 20))
+    print(power.__name__)
+    print('*' * 40, 'Calling fibo(10)')
+    print('fibonacci 20 =', fibo(20))
+    print(fibo.__name__)
